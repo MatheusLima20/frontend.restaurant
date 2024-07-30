f28cd4959a06f33ef9b01534b368d41e9d072255
MatheusLima20 <36931078+MatheusLima20@users.noreply.github.com>
2024-07-30 12:09:36 -0300

    remoção da barra do link

--- src/app/config/axios.ts
+++ src/app/config/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'https://restaurants.back.flatheadinteractive.com/';
+const baseURL = 'https://restaurants.back.flatheadinteractive.com';
 
 export default axios.create({
   baseURL: baseURL,
