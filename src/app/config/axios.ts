e950b0bbc3245ebc577ca2f313a79b30605491ef
MatheusLima20 <36931078+MatheusLima20@users.noreply.github.com>
2024-07-30 12:09:17 -0300

    link

--- src/app/config/axios.ts
+++ src/app/config/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://10.90.1.118:3333';
+const baseURL = 'https://restaurants.back.flatheadinteractive.com/';
 
 export default axios.create({
   baseURL: baseURL,
