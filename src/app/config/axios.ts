f4469a42788509e6d005069f57f8338859288a66
MatheusLima20 <36931078+MatheusLima20@users.noreply.github.com>
2024-09-20 08:03:54 -0300

    build

--- src/app/config/axios.ts
+++ src/app/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const baseURL = import.meta.env.VITE_BASE_URL;
-console.log(baseURL);
+console.log(baseURL, ' baseURL');
 export default axios.create({
   baseURL: baseURL,
 });
